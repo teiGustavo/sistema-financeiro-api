4202cce34149440a2010a19310a7abf871d6a9c3
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2024-02-04 20:11:23 -0300

    :recycle: refactor: Alter GetAllProvider response

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -10,9 +10,11 @@ import { EmpresasRouter, DocumentosRouter, TransacoesRouter } from './routes';
 const server = express();
 
 
-server.use(cors({
-    origin: process.env.ENABLED_CORS?.split(';') || '*'
-}));
+// server.use(cors({
+//     origin: process.env.ENABLED_CORS?.split(';') || '*'
+// }));
+
+server.use(cors());
 
 server.use(express.json());
 
--- src/server/database/providers/transacoes/GetAll.ts
+++ src/server/database/providers/transacoes/GetAll.ts
@@ -1,12 +1,34 @@
 import { ETableNames } from '../../ETableNames';
 import { Knex } from '../../knex';
-import { ITransacao } from '../../models';
 
 
-export const getAll = async (page: number, limit: number, filter: string, id = 0): Promise<ITransacao[] | Error> => {
+interface IFormattedTransacao {
+    id: number;
+    data?: string;
+    valor?: number;
+    descricao?: string;
+    tipo?: string;
+    situacao?: string;
+    documento_id: number;
+    empresa_pagadora_id: number;
+    empresa_recebedora_id: number;
+}
+
+
+export const getAll = async (page: number, limit: number, filter: string, id = 0): Promise<IFormattedTransacao[] | Error> => {
     try {
         const result = await Knex(ETableNames.transacoes)
-            .select('*')
+            .select(
+                'id', 
+                Knex.raw('DATE_FORMAT(data, "%d/%m/%Y") as data'), 
+                Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(valor, 2), ".", "|"), ",", "."), "|", ",")) as valor'), 
+                'descricao', 
+                'tipo', 
+                'situacao', 
+                'documento_id', 
+                'empresa_pagadora_id', 
+                'empresa_recebedora_id'
+            )
             .where('id', Number(id))
             .orWhere('data', 'like', `%${filter}%`)
             .offset((page - 1) * limit)
@@ -14,7 +36,17 @@ export const getAll = async (page: number, limit: number, filter: string, id = 0
 
         if (id > 0 && result.every((item) => item.id !== id)) {
             const resultById = await Knex(ETableNames.transacoes)
-                .select('*')
+                .select(
+                    'id', 
+                    Knex.raw('DATE_FORMAT(data, "%d/%m/%Y") as data'), 
+                    Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(valor, 2), ".", "|"), ",", "."), "|", ",")) as valor'), 
+                    'descricao', 
+                    'tipo', 
+                    'situacao', 
+                    'documento_id', 
+                    'empresa_pagadora_id', 
+                    'empresa_recebedora_id'
+                )
                 .where('id', id)
                 .first();
 

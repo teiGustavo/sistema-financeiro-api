22d2ba1e9b5518c055223cfeb0ec57416cdfcad8
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2024-02-05 01:38:02 -0300

    :recycle: refactor: Alter GetAllProvider response v2

--- src/server/database/providers/transacoes/GetAll.ts
+++ src/server/database/providers/transacoes/GetAll.ts
@@ -2,58 +2,120 @@ import { ETableNames } from '../../ETableNames';
 import { Knex } from '../../knex';
 
 
+interface IResultTransacao {
+    id: number;
+    data: string;
+    valor: number;
+    descricao: string;
+    tipo: string;
+    situacao: string;
+    documento_id: number;
+    documento_tipo: string;
+    empresa_pagadora_id: number;
+    empresa_pagadora_nome: string;
+    empresa_recebedora_id: number;
+    empresa_recebedora_nome: string;
+}
+
+
 interface IFormattedTransacao {
     id: number;
-    data?: string;
-    valor?: number;
-    descricao?: string;
-    tipo?: string;
-    situacao?: string;
-    documento_id: number;
-    empresa_pagadora_id: number;
-    empresa_recebedora_id: number;
+    data: string;
+    valor: number;
+    descricao: string;
+    tipo: string;
+    situacao: string;
+    documento: {
+        id: number,
+        tipo: string
+    };
+    empresa_pagadora: {
+        id: number;
+        nome: string;
+    };
+    empresa_recebedora: {
+        id: number;
+        nome: string;
+    };
 }
 
 
 export const getAll = async (page: number, limit: number, filter: string, id = 0): Promise<IFormattedTransacao[] | Error> => {
     try {
-        const result = await Knex(ETableNames.transacoes)
+        const result: IResultTransacao[] = await Knex(`${ETableNames.transacoes} AS T`)
             .select(
-                'id', 
-                Knex.raw('DATE_FORMAT(data, "%d/%m/%Y") as data'), 
-                Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(valor, 2), ".", "|"), ",", "."), "|", ",")) as valor'), 
-                'descricao', 
-                'tipo', 
-                'situacao', 
-                'documento_id', 
-                'empresa_pagadora_id', 
-                'empresa_recebedora_id'
+                'T.id', 
+                'T.data',
+                'T.valor',
+                Knex.raw('DATE_FORMAT(T.data, "%d/%m/%Y") AS data'), 
+                Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(T.valor, 2), ".", "|"), ",", "."), "|", ",")) AS valor'), 
+                'T.descricao', 
+                'T.tipo', 
+                'T.situacao', 
+                'T.documento_id', 
+                'D.tipo AS documento_tipo', 
+                'T.empresa_pagadora_id', 
+                'EP.nome AS empresa_pagadora_nome', 
+                'T.empresa_recebedora_id',
+                'ER.nome AS empresa_recebedora_nome'
             )
-            .where('id', Number(id))
-            .orWhere('data', 'like', `%${filter}%`)
+            .join(`${ETableNames.documentos} AS D`, 'T.documento_id', 'D.id')
+            .join(`${ETableNames.empresas} AS EP`, 'T.empresa_pagadora_id', 'EP.id')
+            .join(`${ETableNames.empresas} AS ER`, 'T.empresa_recebedora_id', 'ER.id')
+            .where('T.id', Number(id))
+            .orWhere('T.data', 'like', `%${filter}%`)
             .offset((page - 1) * limit)
             .limit(limit);
 
         if (id > 0 && result.every((item) => item.id !== id)) {
-            const resultById = await Knex(ETableNames.transacoes)
+            const resultById = await Knex(`${ETableNames.transacoes} AS T`)
                 .select(
-                    'id', 
-                    Knex.raw('DATE_FORMAT(data, "%d/%m/%Y") as data'), 
-                    Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(valor, 2), ".", "|"), ",", "."), "|", ",")) as valor'), 
-                    'descricao', 
-                    'tipo', 
-                    'situacao', 
-                    'documento_id', 
-                    'empresa_pagadora_id', 
-                    'empresa_recebedora_id'
+                    'T.id', 
+                    'T.data',
+                    'T.valor',
+                    Knex.raw('DATE_FORMAT(T.data, "%d/%m/%Y") AS data'), 
+                    Knex.raw('CONCAT("R$ ", REPLACE(REPLACE(REPLACE(FORMAT(T.valor, 2), ".", "|"), ",", "."), "|", ",")) AS valor'), 
+                    'T.descricao', 
+                    'T.tipo', 
+                    'T.situacao', 
+                    'T.documento_id', 
+                    'D.tipo AS documento_tipo', 
+                    'T.empresa_pagadora_id', 
+                    'EP.nome AS empresa_pagadora_nome', 
+                    'T.empresa_recebedora_id',
+                    'ER.nome AS empresa_recebedora_nome'
                 )
-                .where('id', id)
+                .join(`${ETableNames.documentos} AS D`, 'T.documento_id', 'D.id')
+                .join(`${ETableNames.empresas} AS EP`, 'T.empresa_pagadora_id', 'EP.id')
+                .join(`${ETableNames.empresas} AS ER`, 'T.empresa_recebedora_id', 'ER.id')
+                .where('T.id', id)
                 .first();
 
             if (resultById) return [...result, resultById];
         }
 
-        return result;
+        return result.map((transacao) => {
+            return {
+                id: transacao.id,
+                data: transacao.data,
+                valor: transacao.valor,
+                descricao: transacao.descricao,
+                tipo: transacao.tipo,
+                situacao: transacao.situacao,
+                documento: {
+                    id: transacao.documento_id,
+                    tipo: transacao.documento_tipo
+                },
+                empresa_pagadora: {
+                    id: transacao.empresa_pagadora_id,
+                    nome: transacao.empresa_pagadora_nome
+                },
+                empresa_recebedora: {
+                    id: transacao.empresa_recebedora_id,
+                    nome: transacao.empresa_recebedora_nome
+                }
+            };
+        });
     } catch (error) {
         console.log(error);
         
